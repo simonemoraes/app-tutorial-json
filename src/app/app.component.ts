581bee6ca85bb5bd728396c1a4d5917496deae8e
Simone Moraes <simone.moraes@gmail.com>
2017-12-14 14:25:18 -0200

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Frete } from './Frete';
+import { FreteService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  frete: Frete;
+  cep: string;
+
+  constructor( private _freteService: FreteService ) {}
+
+  CalcularFrete(): void {
+    this._freteService.getFrete( this.cep )
+    .subscribe( (data: Frete) => this.frete = data,
+    error => console.log( error ));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FreteService } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [FreteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
